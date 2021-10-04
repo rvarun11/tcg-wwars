be87d643353f46415037f39a1f1633065350e2d5
Varun Rajput <rvarun011@gmail.com>
2021-10-04 14:33:33 +0530

    connect with FAST API()

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/shomali11/slacker"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"tcg-wwars/helpers"
 )
+
+type Query struct {
+	Body string `json:"body"`
+}
+
+type Reply struct {
+	Body string `json:"body"`
+}
 
 func printCmdEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 	for event := range analyticsChannel {
@@ -21,20 +33,25 @@ func printCmdEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 }
 
 func main() {
+	serviceUrl := "http://localhost:8000/"
+
 	helpers.SetTokens()
+
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 
-	go printCmdEvents(bot.CommandEvents())
-
-	// 1. Take any input and repeat it back
+	//go printCmdEvents(bot.CommandEvents())
 
 	definition := &slacker.CommandDefinition{
+		Description: "ask Ask a question",
+		Example:     "ask How far is the sun?",
 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
-			response.Reply("pong")
+			query := request.Param("query")
+			reply := getReply(serviceUrl, query)
+			response.Reply(reply)
 		},
 	}
 
-	bot.Command("ping", definition)
+	bot.Command("ask <query>", definition)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -43,4 +60,26 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getReply(serviceUrl string, q string) string {
+	fmt.Println("Connecting to FastAPI at: ", serviceUrl)
+	query := &Query{Body: q}
+	jsonBytes, _ := json.Marshal(query)
+
+	res, err := http.Post(serviceUrl, "application/json", bytes.NewBuffer(jsonBytes))
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	body, _ := ioutil.ReadAll(res.Body)
+	//fmt.Println("Response Status: ", res.Status)
+	//fmt.Println("Response Headers: ", res.Header)
+	//fmt.Println("Response Body: ", string(body))
+
+	reply := Reply{}
+	_ = json.Unmarshal(body, &reply)
+
+	return reply.Body
 }
